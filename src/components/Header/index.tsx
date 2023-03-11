95043ef3f1abc0d040f2bf7dba0b4d81548d3254
Bruna Flôr <brn.flor@gmail.com>
2023-03-11 16:24:14 +0000

    searchbox just in wideversion

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,7 +14,7 @@ const isWideVersion = useBreakpointValue({
         <Flex as="header" w="100%" maxWidth={1480} h="20" mx="auto" mt="4" px="6" align="center">
             <Logo/>
 
-            <SearchBox/>
+            {isWideVersion && <SearchBox/>}
             <Flex align="center" ml="auto">
                 <Profile showProfileData={isWideVersion}/>
                 <NotificationsNav />
