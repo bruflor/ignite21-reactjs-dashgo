f44296e36d794dcfc6373f66add41154db953b19
Bruna Flôr <brn.flor@gmail.com>
2023-03-11 16:22:37 +0000

    header responsible

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,22 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./Notifications";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+})
+
     return (
         <Flex as="header" w="100%" maxWidth={1480} h="20" mx="auto" mt="4" px="6" align="center">
             <Logo/>
 
             <SearchBox/>
             <Flex align="center" ml="auto">
-                <Profile />
+                <Profile showProfileData={isWideVersion}/>
                 <NotificationsNav />
             </Flex>
         </Flex>
