9eea48f38f0b7631086c1c937441e0531d4b8880
Bruna Flôr <brn.flor@gmail.com>
2023-03-15 20:41:24 +0000

    changing the structure of form to hook form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
 import { Input } from "@/components/Form/Input";
 import { Flex, Button, Stack, } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form/dist/types";
 
+type SignInFormData= {
+    email: string,
+    password: string
+}
 
 export default function SignIn() {
+const {register, handleSubmit, formState} = useForm()
+
+const handleSignIn: SubmitHandler<SignInFormData> = (data, event) =>{
+console.log(data)
+}
     return (
         <Flex w="100vw" h="100vh" align="center" justify='center'>
-            <Flex as="form" width="100%" maxWidth={360} bg="gray.800" p="8" borderRadius={8} flexDir="column">
+            <Flex as="form" width="100%" maxWidth={360} bg="gray.800" p="8" borderRadius={8} flexDir="column"  onSubmit={handleSubmit(handleSignIn)}>
                 
                 <Stack spacing="4">
-                    <Input name="email" type="email" label="E-mail"/>
-                    <Input name="password" type="password" label="Senha"/>
+                    <Input type="email" label="E-mail" {...register('email')}/>
+                    <Input  type="password" label="Senha" {...register('password')}/>
                 </Stack>
                 
                 <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
