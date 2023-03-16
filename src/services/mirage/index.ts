1171373c5f0ec7b408d5c83d212349292ed3accb
Bruna Flôr <brn.flor@gmail.com>
2023-03-16 18:55:12 +0000

    mirage with fake

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,5 @@
-import {createServer, Model} from "miragejs"
+import { createServer, Factory, Model } from "miragejs"
+import faker from 'faker'
 
 type User = {
     name: string;
@@ -6,17 +7,43 @@ type User = {
     created_at: string;
 }
 
-export function makeServer(){
-const server = createServer({
-    models: {
-        user: Model.extend<Partial<User>>({})
-    },
+export function makeServer() {
+    const server = createServer({
+        models: {
+            user: Model.extend<Partial<User>>({})
+        },
 
-    routes(){
-        this.get('/users');
-        this.post('/users');
-    }
-})
+        factories:{
+            user: Factory.extend(
+                {
+                    name(i:number){
+                        return `User ${i+1}`
+                    },
+                    email(){
+                        return faker.internet.email().toLowerCase()
+                    },
+                    createdAt(){
+                        return faker.date.recent(10)
+                    }
+            }
+        )
 
-return server
+        },
+
+        seeds(server){
+server.createList('user', 200)
+        },
+
+        routes() {
+            this.namespace = "api";
+            this.timing = 750;
+
+            this.get('/users');
+            this.post('/users');
+
+            this.namespace = ''
+        }
+    })
+
+    return server
 }
